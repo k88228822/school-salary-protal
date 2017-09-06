ae1e2b897a6b5ef7bc9891010d0da701ffc7ef97
wangzhen <870190671@qq.com>
2017-09-06 12:13:42 +0800

    end

--- src/components/temp/TempComponent.js
+++ src/components/temp/TempComponent.js
@@ -8,16 +8,23 @@ function TempComponent() {
       <div className={styles.imageContainer}>
         <img  className={styles.image} src={require('../../assets/first_bg.jpg')}/>
       </div>
-      <Button onClick={()=>{
-        window.open('http://jcc.neu.edu.cn/northEastUcw/index.jsp')
-      }} className={styles.button} type={"primary"}>全民职工工资查询</Button>
+      <div className={styles.selectContainer}>
+        <Button  size={"small"}onClick={()=>{
+          window.open('http://jcc.neu.edu.cn/northEastUcw/index.jsp')
+        }} className={styles.button} type={"primary"}>
+          <span className={styles.textSpan}>全民职工工资查询</span>
+        </Button>
 
-      <Button onClick={()=>{
-        let url= window.location.href.includes('10.0.0.1')?
-          'http://10.0.0.1:3000/app/login':
-          'http://202.118.27.233:3000/app/login'
-        window.open(url)
-      }} className={styles.button} type={"primary"}>人才派遣职工工资查询</Button>
+        <Button size={"small"} onClick={()=>{
+          let url= window.location.href.includes('10.0.0.1')?
+            'http://10.0.0.1:3000/app/login':
+            'http://202.118.27.233:3000/app/login'
+          window.open(url)
+        }} className={styles.button} type={"primary"}>
+          <span className={styles.textSpan}>人才派遣职工工资查询</span>
+        </Button>
+
+      </div>
     </div>
   );
 }
